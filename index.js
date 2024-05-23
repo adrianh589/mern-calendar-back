0813bdd9d286a32027615b474293d60a836b14ee
Adrian Hoyos <adrianh_589@hotmail.com>
2024-05-23 10:33:51 -0500

    Redireccion al recargar la pagina para el index de react agregado

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.use(express.json()); // Analiza el cuerpo de las solicitudes entrantes con f
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+// Cualquier ruta no coincidente, en este caso react que no funciona al recargar la pagina, le dara control a react sobre las rutas
+app.use('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`)
